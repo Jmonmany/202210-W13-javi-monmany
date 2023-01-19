b7315ac3c8b65d88e7028e9c7dfd286145a48c6b
Javi <monmany1992@gmail.com>
2023-01-19 12:09:04 +0100

    Added styles

--- src/features/pages/home/home.tsx
+++ src/features/pages/home/home.tsx
@@ -2,13 +2,13 @@ import { Link } from 'react-router-dom';
 export default function Home() {
     return (
         <>
-            <div className="home-container" role="main">
+            <div className="home" role="main">
                 <Link
                     to={'/characters'}
                     className="material-symbols-outlined btn"
                     title="to Characters"
                 >
-                    person_add
+                    group
                 </Link>
             </div>
         </>
